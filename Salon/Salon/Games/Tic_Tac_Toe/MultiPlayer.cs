3db41ec6d60682971525ad1f6281b7ff2b33f138
Michelle Curry <michellelicurry@icloud.com>
2023-05-11 21:58:41 -0400

    added in tictactaoe (TTT) gameplay made by me

---
+++ Salon/Salon/Games/Tic_Tac_Toe/MultiPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salon.Games.Tic_Tac_Toe
+{
+    internal class MultiPlayer
+    {
+    }
+}
---
+++ Salon/Salon/Games/Tic_Tac_Toe/StartPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Salon.Games.Tic_Tac_Toe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        private static char PlayerIcon { get; set; }
+        public StartPage()
+        {
+            InitializeComponent();
+            PlayerIcon = 'X';
+        }
+        public void single_player_clicked(object sender, EventArgs args)
+        {
+            SetPlayerIcon();
+            App.Current.MainPage = new GameBoard(PlayerIcon, "sp");
+            Console.WriteLine("Single Player Clicked");
+        }
+
+        public void multi_player_clicked(object sender, EventArgs args)
+        {
+            SetPlayerIcon();
+            App.Current.MainPage = new GameBoard(PlayerIcon, "mp");
+            Console.WriteLine("Multi Player Clicked");
+        }
+
+        private void SetPlayerIcon()
+        {
+            if (XIcon.IsChecked)
+            {
+                PlayerIcon = 'X';
+            }
+            else
+            {
+                PlayerIcon = 'O';
+            }
+
+        }
+    }
+}
